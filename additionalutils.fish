5ac01c63ba289b250ac263e08bac684cc6437461
VistaBlue <nvme12@proton.me>
2026-02-16 15:39:49 +0700

    Update and rename autocpufreq.fish to additionalutils.fish

---
+++ additionalutils.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+# Installing auto-cpufreq from the aur
+if paru -S --noconfirm auto-cpufreq libxnvctrl-580xx nvidia-580xx-dkms nvidia-580xx-utils nvidia-580xx-settings lib32-nvidia-580xx-utils opencl-nvidia-580xx lib32-opencl-nvidia-580xx
+    figlet "Nvidia drivers & autocpufreq installed successfully "
+else
+    figlet "Error: Nvidia driver & autocpufreq installation failed"
+    exit 1
+end   
--- autocpufreq.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-# Installing auto-cpufreq from the aur
-if paru -S --noconfirm auto-cpufreq
-    figlet "Installation in progress"
-else
-    figlet "Error: auto-cpufreq installation failed"
-    exit 1
-end   
