1180047cd0a23bbe63f0402f7180009e3c69a356
VistaBlue <nvme12@proton.me>
2026-02-16 15:40:04 +0700

    Update additionalutils.fish

--- additionalutils.fish
+++ additionalutils.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-# Installing auto-cpufreq from the aur
+# Installing additional utils from the aur
 if paru -S --noconfirm auto-cpufreq libxnvctrl-580xx nvidia-580xx-dkms nvidia-580xx-utils nvidia-580xx-settings lib32-nvidia-580xx-utils opencl-nvidia-580xx lib32-opencl-nvidia-580xx
     figlet "Nvidia drivers & autocpufreq installed successfully "
 else
